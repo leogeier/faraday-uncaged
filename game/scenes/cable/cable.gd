72ebf5f38123ec2dedf7a19fa6ad09096b0ce2c7
leogeier <contact@leonardgeier.com>
2022-10-01 15:03:12 +0200

    correct physics for cable

--- game/scenes/cable/cable.gd
+++ game/scenes/cable/cable.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-var vertexA
-var vertexB
+var vertex_a
+var vertex_b
 
 func get_other_vertex(vertex):
-	if vertex == vertexA:
-		return vertexB
-	if vertex == vertexB:
-		return vertexA
-	assert(false)
+	if vertex == vertex_a:
+		return vertex_b
+	if vertex == vertex_b:
+		return vertex_a
+	assert(false, "given vertex is not connected to edge")
+
+func _ready():
+	$PlugB.position.x

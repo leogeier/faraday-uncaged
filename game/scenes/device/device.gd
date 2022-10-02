d8a0cfb1471637b481152ae161ad67f7f929b1b2
leogeier <contact@leonardgeier.com>
2022-10-02 16:18:26 +0200

    device colors

--- game/scenes/device/device.gd
+++ game/scenes/device/device.gd
@@ -1,8 +1,28 @@
 extends Node2D
 
-export(String) var display_name = "Unnamed device"
+export(String) var display_name = "Unnamed device" setget , get_display_name
+export(Color, RGB) var color = Color.white setget set_color
+
+var color_name
 
 var edges = []
+
+var color_dict = {Color.red: 1}
+
+func get_display_name():
+	if color_name != null:
+		return color_name
+	return display_name
+
+func set_color(value):
+	color = value
+	$SpriteColor.modulate = color
+	
+	for c in color_dict.keys():
+		if color.is_equal_approx(c):
+			color_name = color_dict[c]
+			return
+	color_name = null
 
 func get_edges():
 	if $Port.current_plug != null:
@@ -14,9 +34,19 @@ func get_neighbors():
 	for edge in get_edges():
 		neighbors.append(edge.get_other_vertex(self))
 	return neighbors
-
-func get_display_name():
-	return display_name
 	
 func _ready():
+	color_dict[Color.red] = "red"
+	color_dict[Color.blue] = "blue"
+	color_dict[Color.green] = "green"
+	color_dict[Color.yellow] = "yellow"
+	color_dict[Color.orange] = "orange"
+	color_dict[Color.cyan] = "cyan"
+	color_dict[Color.magenta] = "magenta"
+#	color_dict[Color.white] = "white"
+	color_dict[Color.black] = "black"
+	color_dict[Color.gray] = "gray"
+	color_dict[Color.pink] = "pink"
+	
 	$Port.vertex = self
+	set_color(color)

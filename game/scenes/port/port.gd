43cef4a26d7e353dbac9468839115286564cb73f
Julian Arnold <julian.arnold@student.hpi.de>
2022-10-03 20:29:01 +0200

    death screen

--- game/scenes/port/port.gd
+++ game/scenes/port/port.gd
@@ -7,20 +7,24 @@ var current_plug
 signal start_dragging
 signal stopped_dragging
 
+export(Color) var normal_col
+export(Color) var hover_col
+export(Color) var connected_col
+
 func hover_with_plug(_plug):
-	modulate = Color.green
+	modulate = hover_col
 
 func unhover_with_plug(_plug):
-	modulate = Color.white
+	modulate = normal_col
 
 func insert_plug(plug):
-	modulate = Color.blue
+	modulate = connected_col
 	$RemoteTransform2D.remote_path = plug.get_path()
 	current_plug = plug
 	$PlugInsert.play()
 
 func remove_plug(_plug):
-	modulate = Color.green
+	modulate = hover_col
 	$RemoteTransform2D.remote_path = ""
 	current_plug = null
 	$PlugInsert.play()
